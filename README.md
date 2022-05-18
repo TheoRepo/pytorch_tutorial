f03553764d582c71645c481c45c21369d735ede2
Theo Yu <leoyutian@yeah.net>
2022-05-18 15:01:02 +0800

    删除数据文件

--- README.md
+++ README.md
@@ -1,7 +1,36 @@
 # Pytorch框架学习笔记
 
+## 开发环境配置
+1、操作系统：win10
+2、CPU：Core i5-8265U @ 1.60GHz 四核
+3、Anaconda3-4.11.0-Windows-x86_64
+
+安装包要和本地的CPU架构一致
+```bash
+# 查看本机CPU架构
+lscpu
+```
+
+运用conda管理python环境
+```bash
+# 创建虚拟环境
+conda create -n pytorch python=3.9
+
+# 使用conda安装
+conda install pytorch -c pytorch
+conda install torchvision -c pytorch
+conda install matplotlib -c pytorch
+conda install pandas -c pytorch
+
+# 使用python的pip安装
+conda activate pytorch
+pip install torchvision
+```
+
+
 ## 编写深度学习模型基本流程
 在我们要用pytorch构建自己的深度学习模型的时候，基本上都是下面这个流程步骤，写在这里让一些新手童鞋学习的时候有一个大局感觉，无论是从自己写，还是阅读他人代码，按照这个步骤思想（默念4大步骤，找数据定义、找model定义、(找损失函数、优化器定义)，主循环代码逻辑），直接去找对应的代码块，会简单很多。
 
 ## 核心类的使用
 ### datasets&dataloader类的使用
+
--- datasets&dataloader.py
+++ datasets&dataloader.py
@@ -42,5 +42,34 @@ for i in range(1, cols * rows + 1):
     figure.add_subplot(rows, cols, i)
     plt.title(labels_map[label])
     plt.axis("off")
+    # 对数据的维度进行压缩
     plt.imshow(img.squeeze(), cmap="gray")
 plt.show()
+
+
+# creating a custom dataset for your files
+import os
+import pandas as pd
+from torchvision.io import read_image
+
+class CustomImageDataset(Dataset):
+    def __init__(self, annotations_file, img_dir, transform=None, target_transform=None):
+        self.img_labels = pd.read_csv(annotations_file)
+        self.img_dir = img_dir
+        self.transform = transform
+        self.target_transform = target_transform
+
+    def __len__(self):
+        return len(self.img_labels)
+
+    def __getitem__(self, idx):
+        img_path = os.path.join(self.img_dir, self.img_labels.iloc[idx, 0])
+        image = read_image(img_path)
+        label = self.img_labels.iloc[idx, 1]
+        if self.transform:
+            image = self.transform(image)
+        if self.target_transform:
+            label = self.target_transform(label)
+        return image, label
+
+
